c32ae67f90430af46eeed37e016eb7ffcdd658ab
Katsuhide Tsuruta <katsuhide.tsuruta@nulab.com>
2023-06-04 23:27:47 +0900

    add lambda

---
+++ app/event_receiver/build_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+poetry build -f wheel
+poetry export -f requirements.txt --output dist/requirements.txt --without-hashes
+
+poetry run pip install -r dist/requirements.txt --upgrade --only-binary :all: --platform linux-x86_64 --target dist/package/python dist/*.whl
+
+cd dist/package
+
+zip -r -q ../module.zip . -x '*.pyc'
